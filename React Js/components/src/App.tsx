73c8397df292908fd39d520fd66e46464396400f
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-01-29 00:00:05 +0530

    tsx

--- React Js/components/src/App.tsx
+++ React Js/components/src/App.tsx
@@ -1,8 +1,14 @@
 function App() {
+   const handleClick = () => {
+      alert("Button Clicked")
+    }
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="hello">Hello World</h1>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" />
+      <button onClick={handleClick}>Submit</button>
     </>
   )
 }
