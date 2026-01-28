56dd190ee4c5f950fcb5af0e662ca847e0b2937e
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-01-29 00:14:57 +0530

    Understanding TSX Component (TypeScript + React)

--- React Js/components/src/App.tsx
+++ React Js/components/src/App.tsx
@@ -1,14 +1,30 @@
 function App() {
-   const handleClick = () => {
+   const handleClick = () : void => {
       alert("Button Clicked")
+    }
+    const name: string = "World"
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) : void => {
+      console.log(event.target.value)
+    }
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) : void => {
+      event.preventDefault()
+      console.log("Form Submitted")
     }
 
   return (
     <>
       <h1 className="hello">Hello World</h1>
+      <p>name : {name}</p>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" />
+      <input type="text" id="name" onChange={handleChange} required={true} />
       <button onClick={handleClick}>Submit</button>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
     </>
   )
 }
