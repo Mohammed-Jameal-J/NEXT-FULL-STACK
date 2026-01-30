93fe8a9eef14f2a3e78c0e882d05e4ec8a9fb0b0
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-01-30 16:15:12 +0530

    React Hook Form

---
+++ React Js/Hook-Form/src/App.tsx
@@ -0,0 +1,46 @@
+import { useForm , type SubmitHandler  } from "react-hook-form"
+
+type UserFormInputs = {
+  name: string;
+  email: string;
+};
+
+function App() {
+    const { register,reset, handleSubmit, watch , formState: { errors }} = useForm<UserFormInputs>(
+      {
+      defaultValues: {
+        name: "",
+        email: "",
+      }
+    });
+
+    const dataSubmit: SubmitHandler<UserFormInputs> = (data) => {
+        console.log(data);
+        reset();
+    }
+
+    const watchName = watch("name"); 
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(dataSubmit)}>
+        <input type="text" placeholder="Enter name" 
+        {...register("name",{
+          required: "Name is required",
+        })}  />
+        <br />
+        <input type="text" placeholder="Enter email" 
+        {...register("email",{
+          required: "Email is required",
+        })} />
+        {errors.name && <p>{errors.name.message}</p>}
+        {errors.email && <p>{errors.email.message}</p>}
+        <br />
+        <p>Watched name: {watchName}</p>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default App
