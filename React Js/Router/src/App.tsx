ff6442fc96dda927498a8e84f5edadd1d66c0db9
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-02-04 13:30:21 +0530

    Lazy & Suspense

--- React Js/Router/src/App.tsx
+++ React Js/Router/src/App.tsx
@@ -1,23 +1,87 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import User from "./pages/User";
+import { lazy, Suspense } from "react";
 import DefaultLayout from "./layouts/DefaultLayout";
-import Login from "./pages/Login";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const User = lazy(() => import("./pages/User"));
+const Login = lazy(() => import("./pages/Login"));
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/users/:id" element={<User />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<h1>Component Loading</h1>} >
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/users/:id" element={<User />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
 
 export default App;
+
+// and we can add suspense for particular pages too 
+
+// import { Route, Routes } from "react-router-dom";
+// import { lazy, Suspense } from "react";
+// import DefaultLayout from "./layouts/DefaultLayout";
+
+// const Home = lazy(() => import("./pages/Home"));
+// const About = lazy(() => import("./pages/About"));
+// const User = lazy(() => import("./pages/User"));
+// const Login = lazy(() => import("./pages/Login"));
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<DefaultLayout />}>
+
+//         <Route
+//           index
+//           element={
+//             <Suspense fallback={<h2>🏠 Loading Home...</h2>}>
+//               <Home />
+//             </Suspense>
+//           }
+//         />
+
+//         <Route
+//           path="about"
+//           element={
+//             <Suspense fallback={<h2>ℹ️ Loading About...</h2>}>
+//               <About />
+//             </Suspense>
+//           }
+//         />
+
+//         <Route
+//           path="users/:id"
+//           element={
+//             <Suspense fallback={<h2>👤 Loading User...</h2>}>
+//               <User />
+//             </Suspense>
+//           }
+//         />
+
+//         <Route
+//           path="login"
+//           element={
+//             <Suspense fallback={<h2>🔐 Loading Login...</h2>}>
+//               <Login />
+//             </Suspense>
+//           }
+//         />
+
+//       </Route>
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
